668544cd8aed4c499db8f2dd40f244d151c9f45a
MJSuttles <rolltiderolldad@gmail.com>
2025-04-01 15:33:11 -0500

    coded AuthorData.cs, BookData.cs, UserData.cs, and Simply BooksDbContext.cs

---
+++ Data/BookData.cs
@@ -0,0 +1,12 @@
+using SimplyBooks.Models;
+
+namespace SimplyBooks.Data
+{
+  public class BookData
+  {
+    public static List<Book> Books = new()
+    {
+      // insert data
+    };
+  }
+}
---
+++ Data/UserData.cs
@@ -0,0 +1,12 @@
+using SimplyBooks.Models;
+
+namespace SimplyBooks.Data
+{
+  public class UserData
+  {
+    public static List<User> Users = new()
+    {
+      // insert data
+    };
+  }
+}
